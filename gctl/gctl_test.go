9574186aa5c3b726bf72cd5d5264af46c6ff6b79
Nick Patavalis <npat@efault.net>
2015-10-25 23:02:57 +0200

    gctl: More tests

--- gctl/gctl_test.go
+++ gctl/gctl_test.go
@@ -164,3 +164,124 @@ func TestKillWait(t *testing.T) {
 		t.Fatalf("Gcx.Wait: %v", e)
 	}
 }
+
+func TestGroupUninit(t *testing.T) {
+	var c *Gcx
+	var xs error
+	var g Group
+	c, xs = g.Wait()
+	if c != nil || xs != nil {
+		t.Fatalf("g.Wait: c = %p, xs = %v", c, xs)
+	}
+	c, xs = g.Poll()
+	if c != nil || xs != nil {
+		t.Fatalf("g.Poll: c = %p, xs = %v", c, xs)
+	}
+	if n := g.Count(); n != 0 {
+		t.Fatalf("g.Count: n = %d", n)
+	}
+}
+
+func TestGroupSet(t *testing.T) {
+	var g Group
+	var c Gcx
+	var x *Gcx
+	var xs error
+
+	c.Go(func() error { return nil })
+	if e := c.SetGroup(&g); e != ErrStarted {
+		t.Fatalf("Gcx.SetGroup: %v", e)
+	}
+	c.Wait()
+	c = GcxZero
+
+	if e := c.SetGroup(&g); e != nil {
+		t.Fatalf("Gcx.SetGroup: %v", e)
+	}
+	if n := g.Count(); n != 0 {
+		t.Fatalf("Group.Count: %d", n)
+	}
+	x, xs = g.Wait()
+	if x != nil || xs != nil {
+		t.Fatalf("g.Wait: x = %p, xs = %v", x, xs)
+	}
+
+	c.Go(func() error { return nil })
+	if n := g.Count(); n != 1 {
+		t.Fatalf("Group.Count: %d", n)
+	}
+	x, xs = g.Wait()
+	if x != &c || xs != nil {
+		t.Fatalf("g.Wait: x = %p, xs = %v", x, xs)
+	}
+	x, xs = g.Wait()
+	if x != nil || xs != nil {
+		t.Fatalf("g.Wait: x = %p, xs = %v", x, xs)
+	}
+	if n := g.Count(); n != 0 {
+		t.Fatalf("Group.Count: %d", n)
+	}
+}
+
+func TestGroupMany(t *testing.T) {
+	N := 42
+	g := &Group{}
+	fn := func(fn func() error) error {
+		for i := 0; i < N; i++ {
+			wms := time.Duration(rand.Intn(10) + 1)
+			time.Sleep(wms * time.Millisecond)
+			c := &Gcx{}
+			c.SetGroup(g)
+			c.Go(fn)
+		}
+		return nil
+	}
+	fnX := func() error {
+		wms := time.Duration(rand.Intn(100) + 1)
+		time.Sleep(wms * time.Millisecond)
+		return nil
+	}
+	fn1 := func() error {
+		fn(fnX)
+		return nil
+	}
+	fn0 := func() error {
+		fn(fn1)
+		return nil
+	}
+
+	go fn0()
+
+	time.Sleep(200 * time.Millisecond)
+	n := 0
+
+	// Grab a few
+	for c, xs := g.Poll(); c != nil; c, xs = g.Poll() {
+		if xs != nil {
+			t.Fatalf("%d: %v", n, xs)
+		}
+		n++
+	}
+	//n1 := n
+
+	// Wait for all to start
+	time.Sleep(4 * time.Second)
+
+	if cnt := g.Count(); cnt != (N+N*N)-n {
+		t.Fatalf("g.Count %d != %d", cnt, (N+N*N)-n)
+	}
+
+	for c, xs := g.Wait(); c != nil; c, xs = g.Wait() {
+		if xs != nil {
+			t.Fatalf("%d: %v", n, xs)
+		}
+		n++
+		if cnt := g.Count(); cnt != (N+N*N)-n {
+			t.Fatalf("g.Count %d != %d", cnt, (N+N*N)-n)
+		}
+	}
+	if n != N+N*N {
+		t.Fatalf("Waited for %d != %d goroutines", n, N+N*N)
+	}
+	//t.Logf("n1 := %d, n2 = %d, total = %d", n1, n-n1, N+N*N)
+}
