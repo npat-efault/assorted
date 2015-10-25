f00d46c315151ec8e10de239893a77741ec11df2
Nick Patavalis <npat@efault.net>
2015-10-25 21:57:45 +0200

    gctl: Cleanup work + New group methods
    - Renamed sync.Mutex embedded in Gcx and Group to "mu" (this hides it
      from users outside the package)
    
    - Added Group methods Poll and Count
    
    - Group.Wait now returns the exit status of the goroutine along with its
      context
    
    - Lots of comments.

--- gctl/gctl.go
+++ gctl/gctl.go
@@ -18,10 +18,10 @@ var (
 )
 
 // Gcx is a goroutine context. A pointer to a Gcx structure acts like
-// a goroutine id: Identifies a specific goroutine and can be used
-// to start it, kill it, and wait for its exit status.
+// a goroutine id: It identifies a specific goroutine and can be used
+// to start it, kill it, and retrieve its exit status.
 type Gcx struct {
-	sync.Mutex
+	mu       sync.Mutex
 	kill     chan struct{}
 	dead     chan struct{}
 	signaled bool
@@ -37,44 +37,44 @@ var GcxZero Gcx
 // context c, and returns the channel upon which the goroutine should
 // wait for termination requests.
 func (c *Gcx) ChKill() <-chan struct{} {
-	c.Lock()
+	c.mu.Lock()
 	if c.kill == nil {
-		c.Unlock()
+		c.mu.Unlock()
 		panic("Gcx.ChKill: Gcx is not a running goroutine.")
 	}
-	c.Unlock()
+	c.mu.Unlock()
 	return c.kill
 }
 
 // Go runs function f as a goroutine with context c. If a goroutine
 // with context c has already been started, returns with
 // ErrStarted. The goroutine terminates when function f returns. The
-// return value of f is the goroutine's exit status, which is returned
-// by method Gcx.Wait().
+// return value of f is the goroutine's exit status, which can be
+// retrieved using the method Gcx.Wait().
 //
 // Normally, once a goroutine with context c has run (and finished)
 // you cannot start another one with the same context (as this can
-// cause races, if you are not careful). Instead use a new context for
-// each goroutine you start.
+// cause races, if you are not careful). Use a new context for each
+// goroutine you start.
 //
 // CAVEAT: Nevertheless, if you want to reuse the same context, you
-// must first reset it by assigning to it the value GcxZero. In order
-// to do so safely, you must be certain that the goroutine has indeed
-// finished and that no-one will subcequently use the context to refer
-// to the old goroutine. Again, it is easier not to reuse contexts,
-// and in most cases there is no reason to.
+// can, provided that you first reset it by assigning to it the value
+// GcxZero. In order to do so safely, you must be certain that the
+// goroutine has indeed finished and that no-one will subcequently use
+// the context to refer to the old goroutine. Again, it is easier not
+// to reuse contexts, and in most cases there is no reason to.
 func (c *Gcx) Go(f func() error) error {
-	c.Lock()
-	defer c.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	if c.kill != nil {
 		return ErrStarted
 	}
 	c.kill = make(chan struct{})
 	c.dead = make(chan struct{})
 	if c.group != nil {
-		c.group.Lock()
+		c.group.mu.Lock()
 		c.group.n++
-		c.group.Unlock()
+		c.group.mu.Unlock()
 	}
 	go func(c *Gcx, f func() error) {
 		err := f()
@@ -87,6 +87,7 @@ func (c *Gcx) Go(f func() error) error {
 		// Don't access c after this. Goroutine c is dead, and
 		// they are allowed to zero-out c.
 		if g != nil {
+			// This may block until Group.Wait is called.
 			g.notify <- c
 		}
 	}(c, f)
@@ -98,8 +99,8 @@ func (c *Gcx) Go(f func() error) error {
 // has been started, it returns ErrNotStarted. It is ok to call Kill
 // (for the same goroutine) concurrently from multiple goroutines.
 func (c *Gcx) Kill() error {
-	c.Lock()
-	defer c.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	if c.kill == nil {
 		return ErrNotStarted
 	}
@@ -117,12 +118,12 @@ func (c *Gcx) Kill() error {
 // started, it returns ErrNotStarted. It is ok to call Wait (for the
 // same goroutine) concurrently from multiple goroutines.
 func (c *Gcx) Wait() error {
-	c.Lock()
+	c.mu.Lock()
 	if c.kill == nil {
-		c.Unlock()
+		c.mu.Unlock()
 		return ErrNotStarted
 	}
-	c.Unlock()
+	c.mu.Unlock()
 	<-c.dead
 	return c.status
 }
@@ -141,29 +142,51 @@ func (c *Gcx) KillWait() error {
 }
 
 // Group groups together several goroutines. A group is used when one
-// wishes to wait on a group of goroutines and be notified when one
+// wishes to wait on a number of goroutines and be notified when one
 // (any) of them terminates.
+//
+// You can set the group of a goroutine by calling Gcx.SetGroup. A
+// goroutine is considered member of the group from the time it is
+// started (with Gcx.Go) and until it terminates *and* a call to
+// Group.Wait or Group.Poll returns it's context and
+// exit-status. Getting the goroutine's exit status via Gcx.Wait does
+// *not* remove it from the group; a subsequent Group.Wait or
+// Group.Poll call will still return its context and exit status.
+//
+// ATTENTION: If a group is garbage-collected (that is, all references
+// to it are dropped) while is still has goroutines for which their
+// exit status has not been retrieved (by calling Group.Wait or
+// Group.Poll) then you program will leak goroutines. Therefore,
+// before dropping a group, make sure you have retrieved the exit
+// status of all its member goroutines using Group.Wait and/or
+// Group.Poll.
 type Group struct {
-	sync.Mutex
+	mu     sync.Mutex
 	n      int
 	notify chan *Gcx
 }
 
 func (g *Group) init() {
-	g.Lock()
+	g.mu.Lock()
 	if g.notify == nil {
 		g.notify = make(chan *Gcx)
 	}
-	g.Unlock()
+	g.mu.Unlock()
 }
 
-// SetGroup adds goroutine with context c to group g. A goroutine can
-// belong to only one group. A goroutine must be added to the group
-// before it is started with Gcx.Go(). If SetGroup is called for an
-// already running goroutine, it returns ErrStarted.
+// SetGroup sets the group of goroutine with context c to g. A
+// goroutine can belong to only one group (or no group at all). The
+// group of a goroutine must be set before it is started with
+// Gcx.Go(). If SetGroup is called for an already started goroutine,
+// it returns ErrStarted.
+//
+// Once added and started, the goroutine is considered member of the
+// group. It remains so until it terminates *and* Group.Wait or
+// Group.Poll returns it's exit status. See doc of Group.Wait for
+// more.
 func (c *Gcx) SetGroup(g *Group) error {
-	c.Lock()
-	defer c.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	if c.kill != nil {
 		return ErrStarted
 	}
@@ -173,32 +196,71 @@ func (c *Gcx) SetGroup(g *Group) error {
 }
 
 // Wait waits for one (any) of the goroutines in group g to
-// terminate. It returns the context of the goroutine that terminated,
-// and the number of running goroutines remaining in the group. The
-// exit status of the goroutine that terminated can be acquired by
-// subsequently calling the Wait method on the context returned;
-// c.Wait(), in this case, will return immediatelly, since the
-// goroutine has already terminated. If upon entry to Group.Wait the
-// group has no running goroutines, it returns nil, 0
-func (g *Group) Wait() (c *Gcx, n int) {
-	g.Lock()
-	n = g.n
-	g.Unlock()
+// terminate. It returns the context c of the goroutine that
+// terminated, and it's exit status. If upon entry to Group.Wait, the
+// group has no more goroutines, it returns nil, nil.
+//
+// Once Group.Wait (or Group.Poll) returns a goroutine's context and
+// exit status, then (and only then) the goroutine is no longer
+// considered a member of the group.
+func (g *Group) Wait() (c *Gcx, xs error) {
+	g.mu.Lock()
+	n := g.n
+	g.mu.Unlock()
 	if n == 0 {
-		return nil, n
+		return nil, nil
 	}
 	c = <-g.notify
-	g.Lock()
+	g.mu.Lock()
 	g.n--
 	n = g.n
-	g.Unlock()
-	return c, n
+	g.mu.Unlock()
+	return c, c.Wait()
 }
 
-// WaitAll waits for all the goroutines in group g to terminate.
+// Poll checks if a (any) goroutine in g has terminated, and if so
+// returns its context and exit status. If upon entry to Group.Poll
+// there is no goroutine that has terminated (or if the group is
+// empty) it returns nil, nil.
+//
+// Once Group.Poll (or Group.Wait) returns a goroutine's context and
+// exit status, then (and only then) the goroutine is no longer
+// considered a member of the group.
+func (g *Group) Poll() (c *Gcx, xs error) {
+	g.mu.Lock()
+	n := g.n
+	g.mu.Unlock()
+	if n == 0 {
+		return nil, nil
+	}
+	select {
+	case c = <-g.notify:
+	default:
+		return nil, nil
+	}
+	g.mu.Lock()
+	g.n--
+	n = g.n
+	g.mu.Unlock()
+	return c, c.Wait()
+}
+
+// Count returns the number of goroutines in the group (i.e the number
+// of goroutines for which Group.Wait or Group.Poll has not returned
+// their status).
+func (g *Group) Count() int {
+	g.mu.Lock()
+	n := g.n
+	g.mu.Unlock()
+	return n
+}
+
+// WaitAll calls Group.Wait repeatedly until all the goroutines in
+// group g terminate. The exit status'es returned by the repeated
+// calls to Group.Wait are discarded.
 func (g *Group) WaitAll() {
-	_, n := g.Wait()
-	for n != 0 {
-		_, n = g.Wait()
+	c, _ := g.Wait()
+	for c != nil {
+		c, _ = g.Wait()
 	}
 }
